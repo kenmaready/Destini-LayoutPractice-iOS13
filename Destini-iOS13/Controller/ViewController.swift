6718f37409ee1c9e4bcdb7d6fbc958d9323ae558
kenmaready <ken@maready.com>
2022-06-27 15:37:51 -0400

    Added simple story choice and advance functionality, simple StoryManager class to manage

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var manager = StoryManager()
+    
+    @IBOutlet weak var storyTextLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateStoryElements()
     }
 
+    func updateStoryElements() {
+        let currentStory = manager.getCurrentStoryLine()
+        storyTextLabel.text = currentStory.text
+        choice1Button.setTitle( currentStory.choice1.text, for: .normal)
+        choice2Button.setTitle( currentStory.choice2.text, for: .normal)
+    }
 
+    @IBAction func selectPath(_ sender: UIButton) {
+        manager.makeChoice(choice: sender.tag)
+        updateStoryElements()
+    }
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct Choice {
+    let text: String
+    let next: Int
+}
+
+struct StoryLine {
+    let text: String
+    let choice1: Choice
+    let choice2: Choice
+}
+
+let RestartChoice = Choice(text: "Restart game.", next: 0)
+
+let storyLines = [
+    StoryLine(text: "You see a fork in the road.", choice1: Choice(text: "Take a left", next: 1), choice2: Choice(text: "Take a right", next: 2)),
+    StoryLine(text: "You see a tiger.", choice1: Choice(text:"Shout for help", next: 3), choice2: Choice(text: "Play dead", next: 4)),
+    StoryLine(text: "You find a treasure chest.", choice1: Choice(text:"Open it", next: 5), choice2: Choice(text:"Check for traps", next:6)),
+    StoryLine(text: "Help doesn't come, goodbye.", choice1: RestartChoice, choice2: RestartChoice),
+    StoryLine(text: "Tiger eats you. Now you ARE dead, smart guy.", choice1: RestartChoice, choice2: RestartChoice),
+    StoryLine(text: "You should've checked for traps. Goodbye", choice1: RestartChoice, choice2: RestartChoice),
+    StoryLine(text: "You find a trap, dismantle it and the chest opens, revealing all the Gold you can eat!", choice1: RestartChoice, choice2: RestartChoice)
+]
